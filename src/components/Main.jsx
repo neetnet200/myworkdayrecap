3a32394e5767ae783c9ec582cac587bc54257281
misskip <anita.desanmiguel@gmail.com>
2023-02-17 18:32:15 +1100

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import "./main.css";
 import dog_pic_small_square from "./images/dog_pic_small_square.png";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Main() {
-  // let navigate = useNavigate();
+  let navigate = useNavigate();
   return (
     <div className="main">
-       <div className="container">
-      <div className="content">
-       <p>Content here</p>
-       <div className="image-wrap">
-              <img src={dog_pic_small_square} alt="doggy" />
-            </div>
-        {/* <button
-          className="button"
-          onClick={() => {
-            navigate(
-             
-              "/recap"
-            );
-          }}
-        >
-          recap
-        </button> */}
+      <div className="container">
+        <div className="col-1">
+          <div className="image-wrap">
+            <img src={dog_pic_small_square} alt="doggy" />
+          </div>
+        </div>
+
+        <div className="col-2">
+          {/* <h2>Home</h2> */}
+          <div className="col-2-container">
+            {/* <button
+              className="button"
+              onClick={() => {
+                navigate("/recap");
+              }}
+            >
+              recap
+            </button> */}
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
